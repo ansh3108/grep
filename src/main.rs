c4cb55be2cd1a6b593fa95819e75303d5e1b353a
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2026-07-25 18:11:54 +0530

    initial loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{print, println};
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("You are in a dark room");
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        print!("You typed {}\n", input.trim().to_lowercase());
+    }
 }
