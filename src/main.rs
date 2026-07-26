310d9ecc55876daf1ad48ee5008d24729420799c
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2026-07-26 16:39:50 +0530

    refactor main function to use Config struct for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 use std::env;
 use std::fs;
-use std::println;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Raw arguments: {:?}", args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).expect("Problem parsing arguments");
     
-    println!("Searching for: {}", query);
-    println!("In file: {}", file_path);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should've been able to read the file!");
 
     println!("With text:\n{}", contents);
+
+    
 }
+
+
+struct Config {
+        query: String,
+        file_path: String,
+    }
+
+    impl Config {
+        fn build(args: &[String]) -> Result<Config, &'static str> {
+            if args.len() < 3 {
+                return Err("not enough arguments");
+            } else {
+                Ok(Config {
+                    query: args[1].clone(),
+                    file_path: args[2].clone(),
+                })
+            }
+
+        }
+    }
